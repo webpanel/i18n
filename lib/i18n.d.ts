2ad8724a9bddad8ce2a9db2b0e5f75259b064a78
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2021-04-23 21:29:10 +0200

    Remove backend loader

--- lib/i18n.d.ts
+++ lib/i18n.d.ts
@@ -1,7 +1,2 @@
-import { I18NextLocalStorageBackend } from 'i18next-localstorage-backend';
-import i18n, { InitOptions } from 'i18next';
-interface i18nInitializeOptions extends InitOptions {
-    localStorageBackend?: I18NextLocalStorageBackend.BackendOptions;
-}
-export declare const i18nInitialize: (opts?: i18nInitializeOptions) => i18n.i18n;
-export {};
+import i18n from "i18next";
+export declare const i18nInitialize: (opts?: i18n.InitOptions) => i18n.i18n;
--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,24 +1,10 @@
-import LocalStorageBackend, {
-  I18NextLocalStorageBackend
-} from 'i18next-localstorage-backend';
-import i18n, { InitOptions } from 'i18next';
+import i18n, { InitOptions } from "i18next";
 
-import Backend from 'i18next-chained-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import { initReactI18next } from 'react-i18next';
-import xhr from 'i18next-xhr-backend';
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
 
-interface i18nInitializeOptions extends InitOptions {
-  // fallbackLng?: string;
-  // debug?: boolean;
-  localStorageBackend?: I18NextLocalStorageBackend.BackendOptions;
-}
-
-export const i18nInitialize = (opts: i18nInitializeOptions = {}): i18n.i18n => {
+export const i18nInitialize = (opts: InitOptions = {}): i18n.i18n => {
   i18n
-    // load translation using xhr -> see /public/locales
-    // learn more: https://github.com/i18next/i18next-xhr-backend
-    .use(Backend)
     // detect user language
     // learn more: https://github.com/i18next/i18next-browser-languageDetector
     .use(LanguageDetector)
@@ -28,34 +14,17 @@ export const i18nInitialize = (opts: i18nInitializeOptions = {}): i18n.i18n => {
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
       ...{
-        fallbackLng: 'en',
+        fallbackLng: "en",
 
         debug: false,
         returnObjects: true,
-        saveMissing: false
+        saveMissing: false,
       },
       ...opts,
       // compatibilityJSON: 'v1',
-
-      backend: {
-        backends: [
-          LocalStorageBackend, // primary
-          xhr // fallback
-        ],
-        backendOptions: [
-          {
-            expirationTime: 60 * 60 * 1000,
-            ...opts.localStorageBackend
-          },
-          {
-            // loadPath: '/locales/{{lng}}/{{ns}}.json' // xhr load path for my own fallback
-          }
-        ]
-      },
-
       interpolation: {
-        escapeValue: false // not needed for react as it escapes by default
-      }
+        escapeValue: false, // not needed for react as it escapes by default
+      },
     });
 
   return i18n;
